3eb04011f60c2f2bb4cc66313efbf9ca580c8bcd
Prince <anakwaprince204@gmail.com>
2025-12-14 07:05:53 +0000

    Reminder Project

--- config/db.js
+++ config/db.js
@@ -12,7 +12,7 @@ const connectDB = async()=> {
         await mongoose.connect(DB);
         console.log('DB connection successful 👌');
     }catch(err){
-        console.log('ERROR ', err);
+        console.log('Error ', err);
         process.exit(1);
     }
 };
